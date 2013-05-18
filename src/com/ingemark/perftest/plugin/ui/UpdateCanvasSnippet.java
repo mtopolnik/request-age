8d71ac24ef7e925de1e6260bea8d9cecfc886d9e
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-05-18 18:48:50 +0200

    testing redraw

--- src/com/ingemark/perftest/plugin/ui/UpdateCanvasSnippet.java
+++ src/com/ingemark/perftest/plugin/ui/UpdateCanvasSnippet.java
@@ -29,82 +29,114 @@ import org.eclipse.swt.widgets.Shell;
 
 public class UpdateCanvasSnippet extends Composite
 {
-  static final Display display = new Display();
-  static final ScheduledExecutorService sched = newSingleThreadScheduledExecutor();
-  static double phi, phase;
-  boolean rectDrawn;
-  Image drawable = new Image(display,1,1);
-  long numbersLastPrinted;
-  private Canvas canvas;
+   static final Display display = new Display();
+   static final ScheduledExecutorService sched = newSingleThreadScheduledExecutor();
+   static double phi, phase;
+   boolean rectDrawn;
+   Image drawable = new Image(display, 1, 1);
+   long numbersLastPrinted;
+   private Canvas canvas;
 
-  public UpdateCanvasSnippet(Composite parent, int style) {
-    super(parent, style);
-    setLayout(new GridLayout(1, false));
-    canvas = new Canvas(this, SWT.NO_BACKGROUND | SWT.DOUBLE_BUFFERED);
-    gridData().align(FILL, FILL).grab(true, true).applyTo(canvas);
-    canvas.addPaintListener(new PaintListener() {
-      public void paintControl(PaintEvent e) { draw(e.gc); } });
-    canvas.addListener(SWT.Resize, new Listener() { public void handleEvent(Event event) {
-      paintImage(canvas);
-    }});
-  }
+   public UpdateCanvasSnippet(Composite parent, int style)
+   {
+      super(parent, style);
+      setLayout(new GridLayout(1, false));
+      canvas = new Canvas(this, SWT.NO_BACKGROUND | SWT.DOUBLE_BUFFERED);
+      gridData().align(FILL, FILL).grab(true, true).applyTo(canvas);
+      canvas.addPaintListener(new PaintListener()
+      {
+         public void paintControl(PaintEvent e)
+         {
+            draw(e.gc);
+         }
+      });
+      canvas.addListener(SWT.Resize, new Listener()
+      {
+         public void handleEvent(Event event)
+         {
+            paintImage(canvas);
+         }
+      });
+   }
 
-  void paintImage(Canvas canvas) {
-    drawable.dispose();
-    drawable = new Image(display, canvas.getBounds().width, canvas.getBounds().height);
-    final GC gc = new GC(drawable);
-    gc.setForeground(display.getSystemColor(SWT.COLOR_BLUE));
-    final Rectangle area = gc.getClipping();
-    gc.drawRectangle(0, area.height - 51, 50, 50);
-    gc.dispose();
-  }
+   void paintImage(Canvas canvas)
+   {
+      drawable.dispose();
+      drawable = new Image(display, canvas.getBounds().width, canvas.getBounds().height);
+      final GC gc = new GC(drawable);
+      gc.setForeground(display.getSystemColor(SWT.COLOR_BLUE));
+      final Rectangle area = gc.getClipping();
+      gc.drawRectangle(0, area.height - 51, 50, 50);
+      gc.dispose();
+   }
 
-  void draw(GC gc) {
-    if (drawable == null) paintImage(canvas);
-    gc.drawImage(drawable, 0, 0);
-    gc.setForeground(display.getSystemColor(SWT.COLOR_RED));
-    gc.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
-    final Rectangle area = gc.getClipping();
-    for (int x = 0; x < area.width; x++)
-      gc.drawLine(x, 0, x, (int) (area.height / 2 - 30 * sin((phi / 20 - phase - x) / 10d)));
-  }
+   void draw(GC gc)
+   {
+      if (drawable == null)
+         paintImage(canvas);
+      gc.drawImage(drawable, 0, 0);
+      gc.setForeground(display.getSystemColor(SWT.COLOR_RED));
+      gc.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
+      final Rectangle area = gc.getClipping();
+      for (int x = 0; x < area.width; x++)
+         gc.drawLine(x, 0, x, (int) (area.height / 2 - 30 * sin((phi / 20 - phase - x) / 10d)));
+   }
 
-  public static void main(String[] args) throws InterruptedException {
-    final Shell shell = new Shell(display, SWT.SHELL_TRIM | SWT.DOUBLE_BUFFERED);
-    shell.setLayout(new GridLayout(3, false));
-    final Scale scale = new Scale(shell, SWT.NONE);
-    gridData().align(GridData.FILL, GridData.BEGINNING).grab(true, false).applyTo(scale);
-    scale.setMinimum(100);
-    scale.setMaximum(600);
-    scale.addSelectionListener(new SelectionAdapter() {
-      @Override public void widgetSelected(SelectionEvent e) {
-        phase = scale.getSelection();
+   public static void main(String[] args) throws InterruptedException
+   {
+      final Shell shell = new Shell(display, SWT.SHELL_TRIM | SWT.DOUBLE_BUFFERED);
+      shell.setLayout(new GridLayout(3, true));
+      final Scale scale = new Scale(shell, SWT.NONE);
+      gridData().align(GridData.FILL, GridData.FILL).applyTo(scale);
+      scale.setMinimum(100);
+      scale.setMaximum(200);
+      scale.addSelectionListener(new SelectionAdapter()
+      {
+         @Override
+         public void widgetSelected(SelectionEvent e)
+         {
+            phase = scale.getSelection();
+         }
+      });
+      for (int i = 0; i < 8; i++)
+      {
+         final Composite c = new UpdateCanvasSnippet(shell, SWT.None);
+         gridData().grab(true, true).applyTo(c);
       }
-    });
-    for (int i = 0; i < 8; i++) {
-      final Composite c = new UpdateCanvasSnippet(shell, SWT.None);
-      gridData().grab(true, true).applyTo(c);
-    }
-    sched.scheduleAtFixedRate(new Runnable() { public void run() {
-      display.asyncExec(new Runnable() { public void run() {
-        if (shell.isDisposed()) return;
-        phi = System.currentTimeMillis();
-        shell.redraw();
-        shell.update();
-      }
-      });}
-    }, 0, 10, MILLISECONDS);
-   shell.setSize(1000, 800);
-    shell.open();
-    while (!shell.isDisposed()) if (!display.readAndDispatch()) display.sleep();
-    sched.shutdown();
-    sched.awaitTermination(1, SECONDS);
-    display.dispose();
-  }
+      sched.scheduleAtFixedRate(new Runnable()
+      {
+         public void run()
+         {
+            display.asyncExec(new Runnable()
+            {
+               public void run()
+               {
+                  if (shell.isDisposed())
+                     return;
+                  phi = System.currentTimeMillis();
+                  shell.redraw();
+                  shell.update();
+               }
+            });
+         }
+      }, 0, 10, MILLISECONDS);
+      shell.setSize(1000, 800);
+      shell.open();
+      while (!shell.isDisposed())
+         if (!display.readAndDispatch())
+            display.sleep();
+      sched.shutdown();
+      sched.awaitTermination(1, SECONDS);
+      display.dispose();
+   }
 
-  static GridDataFactory gridData() {
-    return GridDataFactory.fillDefaults();
-  }
+   static GridDataFactory gridData()
+   {
+      return GridDataFactory.fillDefaults();
+   }
 
-  @Override protected void checkSubclass() { }
+   @Override
+   protected void checkSubclass()
+   {
+   }
 }
