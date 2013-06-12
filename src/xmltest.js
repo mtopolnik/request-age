5b4b831622a7aeba5c9d3da912a4de19b90b64e7
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-06-12 17:12:20 +0200

    test file

--- src/xmltest.js
+++ src/xmltest.js
@@ -4,18 +4,15 @@ function conf(b) {
 function init() {
    nsdecl("a", "http://www.w3.org/2005/Atom");
    req.acceptableStatus("ok");
-   req.responseDefault("xml");
    test(); 
 }
 
 function test() {
-   req("get").get("http://localhost:8080/g").go(function(r) {
-//      log.debug("get response {}", r.getResponseBody());
-      req("post").post(r.getResponseBody()).body(
-            xml("root", ns("a")).el("child").textel("txt", "g")
+	req("get").get("http://localhost:8080/g").go(function(r) {
+	   req("post").post(r.getResponseBody()).body(
+	         xml("root", ns("a")).el("child").textel("txt", "g")
       ).go(function(r) {
-    	  req("get2").get("http://localhost:8080/"
-    			  + xpath("/a:root/a:child/a:txt/text()").evaluate(r.body())[0])
+    	  req("get2").get(xpath("/a:root/a:child/a:txt/text()").evaluate(r.xmlBody())[0])
     	  .go(null);
       })
    });
