c02b50b886fe27237509fe65d07392c4720befc2
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-09-17 12:56:43 +0200

    Improve p2-ide.sh script

--- p2-ide.sh
+++ p2-ide.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
-~/dev/eclipse/eclipse -application org.eclipse.equinox.p2.director \
+pwd=`pwd`
+~/dev/eclipse/eclipse -noSplash -debug -consoleLog\
+  -application org.eclipse.equinox.p2.director \
   -repository http://download.eclipse.org/releases/kepler/ \
   -installIU org.eclipse.platform.ide,org.eclipse.wst.jsdt.feature.feature.group \
   -tag InitialState \
-  -destination ./reqage-ide/ -bundlepool ./reqage-ide/ \
-  -profile SDKProfile \
+  -destination $pwd/reqage-ide/ -bundlepool $pwd/reqage-ide/ \
+  -profile SDKProfile -roaming\
   -profileProperties org.eclipse.update.install.features=true \
-  -p2.os macosx -p2.ws cocoa -p2.arch x86_64
+  -p2.os $1 -p2.ws $2 -p2.arch $3
