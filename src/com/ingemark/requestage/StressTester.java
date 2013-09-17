0bf8f93b3507cab898362c353efdf24273685c12
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-09-17 16:58:14 +0200

    Configurable JVM arguments

--- src/com/ingemark/requestage/StressTester.java
+++ src/com/ingemark/requestage/StressTester.java
@@ -16,6 +16,7 @@ import static com.ingemark.requestage.Util.sneakyThrow;
 import static com.ingemark.requestage.plugin.RequestAgePlugin.requestAgePlugin;
 import static com.ingemark.requestage.script.JsScope.JS_LOGGER_NAME;
 import static java.lang.Runtime.getRuntime;
+import static java.util.Arrays.asList;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 import static java.util.concurrent.Executors.newScheduledThreadPool;
 import static java.util.concurrent.TimeUnit.MICROSECONDS;
@@ -29,7 +30,9 @@ import static org.jboss.netty.channel.Channels.pipelineFactory;
 import static org.jboss.netty.handler.codec.serialization.ClassResolvers.softCachingResolver;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
@@ -43,6 +46,8 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.jdt.launching.IVMInstall;
 import org.jboss.netty.bootstrap.ClientBootstrap;
@@ -257,13 +262,20 @@ public class StressTester implements Runnable
 
   public static Process launchTester(String scriptFile) {
     try {
+      final List<String> command = new ArrayList<String>();
+      command.add(java());
+      final BufferedReader r = new BufferedReader(new FileReader(scriptFile));
+      final String firstLine = r.readLine();
+      r.close();
+      final Matcher m = Pattern.compile("\\s*//\\s*jvm\\s+(.+)").matcher(firstLine);
+      if (m.matches()) command.addAll(asList(m.group(1).split("\\s+")));
+
       final String bpath = getBundleFile(requestAgePlugin().bundle()).getAbsolutePath();
       final String slash = File.separator;
       final String cp = join(File.pathSeparator, bpath, bpath+slash+"bin", bpath+slash+"lib");
-      log.debug("Launching {} with classpath {}", StressTester.class.getSimpleName(), cp);
-      return new ProcessBuilder(java(), "-Xmx256m", "-XX:+UseConcMarkSweepGC",
-          "-cp", cp, StressTester.class.getName(), scriptFile)
-      .start();
+      command.addAll(asList("-cp", cp, StressTester.class.getName(), scriptFile));
+      log.debug("Launching {}", command);
+      return new ProcessBuilder(command).start();
     } catch (IOException e) { return sneakyThrow(e); }
   }
 
